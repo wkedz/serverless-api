ea40ed4ef6ee22d2a5dea8124cac00f44647772b
Wiesław Kędzierski <w.m.kedzierski@gmail.com>
2025-07-27 13:05:28 +0200

    Add dynamodb.

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-URL=""
-curl -X GET $URL
+STACK_NAME="ServerlessApiStack"
+OUTPUT_KEY="ApiUrl"
+OUTPUT_FILE="./cdk-outputs.json"
+
+if [ ! -f "$OUTPUT_FILE" ]; then
+  echo "Error: File $OUTPUT_FILE not exists."
+  echo "Run:"
+  echo ""
+  echo "    cdk deploy --outputs-file $OUTPUT_FILE"
+  echo ""
+  exit 1
+fi
+
+API_URL=$(jq -r ".\"$STACK_NAME\".\"$OUTPUT_KEY\"" ./cdk-outputs.json)
+
+POST_URL="${API_URL}empl"
+echo "Making POST request to $POST_URL"
+RESPONSE=$(curl -H "Content-Type: application/json" -X POST $POST_URL -d '{"name": "John Doe", "possition": "developer"}')
+id=$(echo "$RESPONSE" | jq -r '.id')
+echo "Extracted id: $id"
+
+
+GET_URL="${API_URL}empl?id=${id}"
+
+echo "Making GET request to $GET_URL"
+curl -X GET $GET_URL
+
+
 echo
--- serverless_api/serverless_api_stack.py
+++ serverless_api/serverless_api_stack.py
@@ -1,8 +1,12 @@
-from aws_cdk import Stack
+from aws_cdk import (
+    Stack,
+    CfnOutput,
+)
 
 from aws_cdk import (
     aws_apigateway,
-    aws_lambda
+    aws_lambda,
+    aws_dynamodb,
 )
 
 from constructs import Construct
@@ -12,13 +16,25 @@ class ServerlessApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        empl_table = aws_dynamodb.TableV2(
+            self,
+            "EmplTable",
+            partition_key=aws_dynamodb.Attribute(
+                name="id", type=aws_dynamodb.AttributeType.STRING
+            ),
+            billing=aws_dynamodb.Billing.on_demand(),
+        )
+
         empl_lambda = aws_lambda.Function(
             self,
             "EmplLambda",
             runtime=aws_lambda.Runtime.PYTHON_3_12,
             code=aws_lambda.Code.from_asset("services"),
-            handler="index.handler"
+            handler="index.handler",
+            environment={"TABLE_NAME": empl_table.table_name},
         )
+
+        empl_table.grant_read_write_data(empl_lambda)
 
         api = aws_apigateway.RestApi(self, "Empl-Api")
         empl_resource = api.root.add_resource("empl")
@@ -28,3 +44,9 @@ class ServerlessApiStack(Stack):
         empl_resource.add_method("POST", empl_lambda_intergation)
 
 
+        CfnOutput(
+            self,
+            "ApiUrl",
+            value=api.url,
+            description="The URL of the API Gateway"
+        )
